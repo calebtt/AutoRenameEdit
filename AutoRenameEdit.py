aa3fe0760023d5f1186d7ceffcf1a71383461688
calebtt <mounteriemachine@hotmail.com>
2022-03-25 20:41:56 -0500

    moved logic to different file, minor improvements and refactor overall.

--- AutoRenameEdit.py
+++ AutoRenameEdit.py
@@ -3,94 +3,22 @@
 import sys
 import os
 import random
+import file_operations as fo
 import copy
 import tkinter as tk
 from tkinter import filedialog
 
-# some const global variables
+from typing import Tuple
+
+PROG_FILE_NAME = sys.argv[0]
+"""name of this python script"""
+
 DICTFILENAME = "dict.txt"
-MAXFILESIZE = 100000000
-PROGRAMFILENAME = "AutoRenameEdit.py"  # The name of this python program...
-EXTENSIONLIST = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+"""name of the dictionary file, no path included."""
+PERIOD_CHAR = "."
+"""named value for period character"""
 # seed random in default manner
 random.seed()
-
-
-#
-#
-# Functions section
-#
-#
-# Builds a list of image files from the list of files in the directory and remove empty entries.
-def build_image_list(filesInDirectory):
-    temp_file_name_list = [""]
-    for stt in filesInDirectory:
-        if stt.endswith(PROGRAMFILENAME):
-            continue
-        elif os.path.isfile(stt):
-            for sp in EXTENSIONLIST:
-                if stt.endswith(sp):
-                    temp_file_name_list.append(stt)
-            # print(stt," found")
-    # Remove any empty strings from the list..
-    temp_file_name_list = list(filter(lambda x: x != "", temp_file_name_list))
-    return temp_file_name_list
-
-
-# Returns true if chosen file name already exists in directory.
-def check_if_file_exists_in_dir(strName):
-    for ext in EXTENSIONLIST:
-        if os.path.exists(strName.rstrip() + ext):
-            return True
-    return False
-
-
-# Some error checking before the dictionary file is loaded into memory.
-def check_dictionary_file(fullPathToDictionary):
-    # some basic error checking regarding the file size of the dictionary file
-    # grabbing file size
-    fileSize = os.path.getsize(fullPathToDictionary)
-    print("Dictionary file size is: ", fileSize, " bytes.")
-    # If the file is less than 50 bytes can it really be a dictionary file?
-    if fileSize < 50:
-        print("Not a dictionary file, too small, exiting...")
-        print("enter to continue")
-        input()
-        sys.exit()
-    # if file size is larger than MAXFILESIZE, exit
-    elif fileSize > MAXFILESIZE:
-        print("File size larger than 100mb, exiting...")
-        print("enter to continue")
-        input()
-        sys.exit()
-
-# read entire dictionary file into memory, return it, removing empty lines, short entries, and odd chars
-def readall_dictionary_file():
-    invalidChars = "@#$%^&*()_+-=|/?:;\\'\",.<>`~"
-    dictionaryFileLines = [""]
-    shortLines = 0
-    with open(dictionaryDirectory + "\\" + DICTFILENAME, "r") as file:
-        tempLine = file.readline()
-        while len(tempLine) > 0:
-            tempLine = tempLine.strip()  # strip ws chars from ends
-            if len(tempLine) > 2: # 3 or more chars
-                for ic in invalidChars: # replace invalid chars in string with space
-                    if tempLine.find(ic):
-                        tempLine = tempLine.replace(ic, ' ')
-                if len(tempLine) > 2:
-                    dictionaryFileLines.append(tempLine)
-            else:
-                shortLines = shortLines + 1
-            tempLine = file.readline()
-    print(shortLines, " short dictionary lines removed...")
-
-    # holds some temporary info
-    emptyLines = len(dictionaryFileLines)
-    # Remove any empty strings from the list.
-    dictionaryFileLines = list(filter(lambda x: x != "", dictionaryFileLines))
-    print(emptyLines - len(dictionaryFileLines), " empty dictionary lines removed...")
-    return dictionaryFileLines
-
 
 #
 #
@@ -104,9 +32,9 @@ dictionaryDirectory = os.getcwd()  # store program dir for finding the dictionar
 workingDirectory = filedialog.askdirectory()  # ask user to browser to the directory with the files.
 directoryList = os.listdir(workingDirectory)  # list the files in the chosen directory.
 os.chdir(workingDirectory)
-
+print("\nTo-be-renamed directory is: ", workingDirectory)
 # build list of image files in directory
-fileNameList = build_image_list(directoryList)
+fileNameList = fo.build_image_list(directoryList, PROG_FILE_NAME)
 if len(fileNameList) > 0:
     print("Built list of original file names...")
     print(len(fileNameList), " files in directory.")
@@ -118,11 +46,16 @@ else:
 
 print("Seeking dictionary file...")
 # Check dictionary file attributes before continuing.
-check_dictionary_file(dictionaryDirectory + "\\" + DICTFILENAME)
+fullDictionaryPath = dictionaryDirectory + "\\" + DICTFILENAME
+goodDictionaryResult = fo.is_dictionary_file_good(fullDictionaryPath)
+if not goodDictionaryResult:
+    print("Not an acceptable dictionary file, exiting...")
+    print("enter to continue")
+    input()
+    sys.exit()
 
 # open dictionary file and read all lines into memory while removing some short, empty, and special chars.
-dictionaryFileLines = readall_dictionary_file()
-
+dictionaryFileLines = fo.readall_dictionary_file(fullDictionaryPath)
 # Verify there are enough new file names in the dictionary list before proceeding.
 if len(dictionaryFileLines) < len(fileNameList):
     print("There are not enough new file names in the dictionary to proceed, exiting...")
@@ -141,20 +74,25 @@ if result.upper() == "Y":
     iCurrentFileNameIndex = 0
     # as long as the number of renamed files is less than existing fileNameList len(), rename and remove entry from dict list
     for currentDictionaryEntry in dictionaryFileLines:
-        if iCurrentFileNameIndex >= len(fileNameList):  # if number of renamed files greater than equal to len of files to rename, break out of loop
+        if iCurrentFileNameIndex >= len(fileNameList):
+            # if number of renamed files greater than equal to len of files to rename, break out of loop
             break
-        if check_if_file_exists_in_dir(currentDictionaryEntry):  # if it exists in the folder already, remove from the dictionary list and move on. # I don't think it's a problem if the existing file happens to have the same name as the chosen new file name.
+        if fo.check_if_file_exists_in_dir(currentDictionaryEntry):
+            # if it exists in the folder already, remove from the dictionary list and move on.
+            # I don't think it's a problem if the existing file happens to have the same name as the chosen new file name.
             dictionaryFileLines.remove(currentDictionaryEntry)
             iCurrentFileNameIndex += 1
             continue
         else:
             currentFileName = fileNameList[iCurrentFileNameIndex]
-            currentNameOnly, currentExtension = os.path.splitext(currentFileName)  # split currentfilename into filename and extension
+            currentNameOnly, currentExtension = os.path.splitext(
+                currentFileName)  # split currentfilename into filename and extension
             os.rename(currentFileName, currentDictionaryEntry.rstrip() + currentExtension)  # rename file.
             print(currentFileName, " has RENAMED to: ", currentDictionaryEntry.rstrip() + currentExtension)
             iCurrentFileNameIndex += 1
-
+else:
+    print("\nAborting...")
 # End of program
-print("\nCOMPLETE.")
+print("COMPLETE.")
 print("enter to continue")
 input()
---
+++ file_operations.py
@@ -0,0 +1,95 @@
+import os
+import sys
+
+# some settings globals
+ALLOWABLE_FNAME_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"""string of allowed filename chars"""
+EXTENSION_LIST = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+"""list of file extensions for image files, includes ."""
+MAX_FILE_SIZE = 100_000_000
+"""in bytes/characters"""
+MIN_FILE_SIZE = 50
+"""in bytes/characters"""
+ENTRY_MIN_FILENAME_LENGTH = 2
+"""minimum length of dictionary entry for a file name"""
+
+def remove_invalid_filename_chars(filenameCandidate):
+    """ Filters a filename candidate for not allowed characters.
+
+    :param filenameCandidate: unfiltered file name, name only no extension or path
+    :return: returns the filename candidate with chars not in the whitelist removed.
+    """
+    filteredName = ""
+    for elem in filenameCandidate:
+        if ALLOWABLE_FNAME_CHARS.find(elem):
+            filteredName += elem
+    return filteredName
+
+def build_image_list(filesInDirectory, programFileName):
+    """Returns a list of image file names from the list of files in the directory and removes empty entries.
+
+    :param filesInDirectory: is a list of all the file names in the directory.
+    """
+    temp_file_name_list = [""]
+    for stt in filesInDirectory:
+        if stt.endswith(programFileName):
+            continue
+        elif os.path.isfile(stt):
+            for sp in EXTENSION_LIST:
+                if stt.endswith(sp):
+                    temp_file_name_list.append(stt)
+            # print(stt," found")
+    # Remove any empty strings from the list..
+    temp_file_name_list = list(filter(lambda x: x != "", temp_file_name_list))
+    return temp_file_name_list
+
+def check_if_file_exists_in_dir(strName):
+    """Returns true if chosen file name already exists in directory.
+
+    :param strName: name of the file to check for existence
+    """
+    for ext in EXTENSION_LIST:
+        if os.path.exists(strName.rstrip() + ext):
+            return True
+    return False
+
+def is_dictionary_file_good(fullPathToDictionary):
+    """some basic error checking regarding the file size of the dictionary file, performed before reading to memory
+
+    :param fullPathToDictionary: full path to the dictionary file
+    :return: True on good file, False on unacceptable file
+    """
+    # grabbing file size
+    fileSize = os.path.getsize(fullPathToDictionary)
+    print("Dictionary file size is: ", fileSize, " bytes.")
+    # If the file is less than 50 bytes can it really be a dictionary file?
+    if fileSize < MIN_FILE_SIZE: #if file size too small
+        return False
+    elif fileSize > MAX_FILE_SIZE: #if file size too large
+        return False
+    return True
+
+def readall_dictionary_file(fullPathToDictionary):
+    """read entire dictionary file into memory, return it, removing empty lines, short entries, and odd chars
+
+    :return: list of dictionary file lines
+    """
+    dictionaryFileLines = [""]
+    shortLines = 0
+    with open(fullPathToDictionary, "r") as file:
+        tempLine = file.readline()
+        while len(tempLine) > 0:
+            tempLine = tempLine.strip()  # strip ws chars from ends
+            tempLine = remove_invalid_filename_chars(tempLine) # remove invalid chars
+            if len(tempLine) > ENTRY_MIN_FILENAME_LENGTH: # filters out names too short
+                dictionaryFileLines.append(tempLine)
+            else:
+                shortLines = shortLines + 1
+            tempLine = file.readline()
+    # holds some temporary info
+    tempLength = len(dictionaryFileLines)
+    # Remove any empty strings from the list.
+    dictionaryFileLines = list(filter(lambda x: x != "", dictionaryFileLines))
+    print(tempLength - len(dictionaryFileLines), " empty dictionary lines removed...")
+    print(shortLines, " short dictionary lines removed...")
+    return dictionaryFileLines
